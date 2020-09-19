18e65fa46b9c05728305277c8d8a714c581b035e
saitejakarri <71328271+saitejakarri@users.noreply.github.com>
2020-09-19 16:31:19 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Web Design and Development - Apssdc'
-copyright = '2020, APSSDC'
-author = 'APSSDC'
+project = 'E-commerce project using java'
+copyright = '2020, teja'
+author = 'teja'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
